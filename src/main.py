66be928fc8455fea5110d0d575129dceb9e8d417
Syed Fatiul Huq <97715174+SyedFatiulHuq@users.noreply.github.com>
2025-01-27 16:47:26 -0800

    Upload 1

---
+++ src/main.py
@@ -0,0 +1,106 @@
+import math
+import os
+
+import context_reader
+import executor
+import transcript_processor as tp
+import utility as util
+import report_selection
+
+
+PROJECTS = ['money manager', 'goose sighting', 'fable']
+RUN_NUM = 5  # the number of runs per transcript
+CAPTION_NUM = 100  # the number of captions per chunk
+
+
+def get_chunk_size(transcript_segments, max_caption_number):
+    total_captions = len(transcript_segments)
+    chunk_num = math.ceil(total_captions / max_caption_number)
+    last_len = total_captions % max_caption_number
+
+    final_len = max_caption_number
+    if chunk_num > 1 and last_len < max_caption_number / 3:
+        padding = math.ceil(last_len / (chunk_num - 1))
+        final_len += padding
+
+    return final_len
+
+
+def get_caption_chunks(transcript_segments, max_caption_number):
+    chunks = []
+    chunk = ""
+    iterate = 0
+    ideal_caption_number = get_chunk_size(transcript_segments, max_caption_number)
+    for segment in transcript_segments:
+        chunk += f"\n{str(segment)}\n"
+        iterate += 1
+
+        if iterate > ideal_caption_number:
+            chunks.append(chunk)
+            chunk = f"\n{str(segment)}\n"
+            iterate = 0
+    if len(chunk) > 0:
+        chunks.append(chunk)
+
+    return chunks
+
+
+def remove_last_blank_line(s):
+    lines = s.split('\n')
+
+    # Check if the last line is blank and remove it if so
+    if lines and lines[-1].strip() == '':
+        lines.pop()
+
+    return '\n'.join(lines)
+
+
+def run_reca11():
+    for project in PROJECTS:
+        run_on_project(project)
+
+
+def run_on_project(project):
+    directory = os.path.join('../artifacts/transcripts', project)
+
+    for file in sorted(os.listdir(directory)):
+        filepath = os.path.join(directory, file)
+        session = file.replace('.vtt', '')
+
+        # for each session in each project, we analyze the transcript and create chunks of the transcript in SpeachSegment objects
+        transcript_segments = tp.extract_segments_from_transcript(filepath)
+        caption_chunks = get_caption_chunks(transcript_segments, CAPTION_NUM)
+
+        print(f"{project} > {file}")
+
+        reports = {}
+
+        # now we feed the llm these chunks 'RUN_NUM' number of times
+        for i in range(RUN_NUM):
+
+            # we extract all the relevant contextual information to add to the prompt
+            context_string = context_reader.get_all_context_string(project, file)
+            print(context_string)
+
+            total_tokens = 0
+            current_report = []
+
+            # we iterate through all the chunks, run the llm with the prompt, where prompt = chunk + contextual info
+            for iteration, chunk in enumerate(caption_chunks):
+                token_count, result = executor.run_model(chunk, context_string)
+
+                # results from individual chunks are collected into one collection
+                current_report.extend(result.issues)
+                total_tokens += token_count
+
+            # print(current_report)
+            reports[i] = current_report
+
+        # from the multiple runs, we conduct report selection and generate the final report
+        report_selection.select_and_print_report(reports, session, project)
+
+        # break  # this break enables only one session per project to be analyzed
+
+
+if __name__ == '__main__':
+    run_reca11()
---
+++ src/report_selection.py
@@ -0,0 +1,146 @@
+import os
+import random
+import copy
+
+from issue_similarity import extract_max_match
+
+
+SIMILARITY_THRESHOLD = .50
+SIMILARITY_THRESHOLD2 = .80
+
+
+def get_random_report_index(reports):
+    return random.choice(list(reports.keys()))
+
+
+def find_report_index_with_max_issues(reports):
+    max_issues = -1
+    reports_with_max_issues = []
+    for key, report in reports.items():
+        issue_count = len(report)
+        if issue_count > max_issues:
+            max_issues = issue_count
+            reports_with_max_issues = [key]
+        elif issue_count == max_issues:
+            reports_with_max_issues.append(key)
+
+    # Check if a file was found with issues
+    if not reports_with_max_issues:
+        return get_random_report_index(reports)
+
+    # Return a random file from the files with the maximum number of issues
+    return random.choice(reports_with_max_issues)
+
+
+def find_report_index_with_min_issues(reports):
+    min_issues = 10000
+    reports_with_min_issues = []
+    for key, report in reports.items():
+        issue_count = len(report)
+        if issue_count < min_issues:
+            min_issues = issue_count
+            reports_with_min_issues = [key]
+        elif issue_count == min_issues:
+            reports_with_min_issues.append(key)
+
+    # Check if a file was found with issues
+    if not reports_with_min_issues:
+        return get_random_report_index(reports)
+
+    # Return a random file from the files with the maximum number of issues
+    return random.choice(reports_with_min_issues)
+
+
+def extract_matching_issue(compared_issue, original_issues):
+    text_to_compare = extract_text(compared_issue)
+
+    for original_issue in original_issues:
+        original_text = extract_text(original_issue)
+        if text_to_compare == original_text:
+            return original_issue
+
+
+def remove_duplicates(issues):
+    i = 0
+    while i < len(issues):
+        issue = issues[i]
+        # print(issue['issue_title'])
+        comparing_issues = issues[i + 1:]
+        # print_titles(comparing_issues)
+        if len(comparing_issues) == 0:
+            break
+
+        base_text = extract_text(issue)
+        texts_to_compare = [extract_text(comparing_issue) for comparing_issue in comparing_issues]
+        max_score, similarity_scores = extract_max_match(base_text, texts_to_compare)
+
+        if max_score >= SIMILARITY_THRESHOLD2:
+            max_score_index = similarity_scores.index(max_score)
+            matching_issue = extract_matching_issue(comparing_issues[max_score_index], issues)
+            # print(f"{issue['issue_title']} <{max_score}> {matching_issue['issue_title']}")
+            issues.remove(matching_issue)
+        i += 1
+
+
+def extract_text(issue):
+    return f"Title: {issue.issue_title}\nDescription: {issue.issue_description}"
+
+
+def compare_new_issues_to_existing(all_issues, new_issues):
+    for existing_issue in all_issues:
+        base_text = extract_text(existing_issue)
+        texts_to_compare = [extract_text(issue) for issue in new_issues]
+        max_score, similarity_scores = extract_max_match(base_text, texts_to_compare)
+
+        if max_score >= SIMILARITY_THRESHOLD:
+            max_score_index = similarity_scores.index(max_score)
+            matching_issue = new_issues[max_score_index]
+            new_issues.remove(matching_issue)
+
+        if len(new_issues) == 0:
+            break
+
+    for remaining_issue in new_issues:
+        all_issues.append(remaining_issue)
+
+
+def find_all_unique_issues(reports, report_max):
+    all_issues = reports.pop(report_max)
+
+    remove_duplicates(all_issues)
+
+    for new_issues in reports.values():
+        # remove_duplicates(file, new_issues)
+        compare_new_issues_to_existing(all_issues, new_issues)
+
+    remove_duplicates(all_issues)
+    return all_issues
+
+
+def create_new_report(issues, filename, project, session):
+    dest_filepath = "../outputs/" + project + "/" + session
+
+    if not os.path.exists(dest_filepath):
+        os.makedirs(dest_filepath)
+
+    with open(os.path.join(dest_filepath, filename), 'w') as file:
+        for issue in issues:
+            file.write(f"issue_title: {str(issue.issue_title)}\n")
+            file.write(f"description: {str(issue.issue_description)}\n")
+            file.write(f"timestamps: {str(issue.timestamps)}\n")
+
+            file.write("\n")
+
+
+def select_and_print_report(reports, session, project):
+
+    report_random = get_random_report_index(reports)
+    report_max = find_report_index_with_max_issues(reports)
+    report_min = find_report_index_with_min_issues(reports)
+
+    issues_all = find_all_unique_issues(copy.deepcopy(reports), report_max)
+
+    create_new_report(reports[report_random], f"{session}-random.txt", project, session)
+    create_new_report(reports[report_max], f"{session}-max.txt", project, session)
+    create_new_report(reports[report_min], f"{session}-min.txt", project, session)
+    create_new_report(issues_all, f"{session}-all.txt", project, session)
